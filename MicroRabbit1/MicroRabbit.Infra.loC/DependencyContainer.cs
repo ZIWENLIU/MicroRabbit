66bb06fb0c5ce1fe16014e3f0e6923e6039d4ca1
ZIWENLIU <42020337+ZIWENLIU@users.noreply.github.com>
2019-12-05 14:01:02 +0800

    Initial Commit of Infrastructure Dependency Project

---
+++ MicroRabbit1/MicroRabbit.Infra.loC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.loC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Domain Bus
+            service.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
